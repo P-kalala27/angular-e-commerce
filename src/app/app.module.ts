6fdfb50982f06d10d2ee5760999bb5f30a6f0254
Princeka27 <jpmulyanga@gmail.com>
2023-12-30 13:06:08 +0100

    angular online shop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainCarouselComponent } from './main-carousel/main-carousel.component';
+import { HomeProductCardComponent } from './home-product-card/home-product-card.component';
+import { ProductSliderComponent } from './product-slider/product-slider.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NavContentComponent } from './navbar/nav-content/nav-content.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSelectModule} from '@angular/material/select';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainCarouselComponent,
+    HomeProductCardComponent,
+    ProductSliderComponent,
+    NavbarComponent,
+    NavContentComponent,
+    ProductsComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
